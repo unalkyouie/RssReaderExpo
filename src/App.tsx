6f8f32a2b536e70baddff7b89b7bb434197df066
aleksandra.pikus@solidstudio.io <alk.pikus@gmail.com>
2025-04-21 14:54:17 +0200

    chore: add basic navigation structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import MainNavigator from './navigation/index';
+
+const queryClient = new QueryClient();
+
+const App=() => {
+  return (
+    <QueryClientProvider client={queryClient}>
+        <MainNavigator />
+    </QueryClientProvider>
+  );
+}
+export default App;
