d0e03599e62370f090a96293d033007a332f376f
aleksandra.pikus@solidstudio.io <alk.pikus@gmail.com>
2025-04-21 20:27:58 +0200

    chore: update package dependencies to include @eva-design/eva, @ui-kitten/components, and react-native-svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
-
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider } from '@ui-kitten/components';
 import MainNavigator from "./navigation/index";
 
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <MainNavigator />
-    </QueryClientProvider>
+    <ApplicationProvider {...eva} theme={eva.light}>
+        <QueryClientProvider client={queryClient}>
+          <MainNavigator />
+        </QueryClientProvider>
+    </ApplicationProvider>
+
   );
 };
 export default App;
